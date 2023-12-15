9a7069edcb56474d462b52dfadef2784dd2dffbf
vijay9711 <vijaysm97@gmail.com>
2023-12-15 12:27:04 -0600

    project structure

---
+++ src/view/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav-bar-vue/>
+    <div class="main-wrapper">
+      <LandingComponentVue/>
+    </div>  
+  </div>
+</template>
+
+<script>
+import LandingComponentVue from '@/components/LandingComponent.vue'
+import NavBarVue from '@/components/NavBar.vue'
+
+export default {
+  components:{
+    LandingComponentVue,
+    NavBarVue
+  }
+}
+</script>
+
+<style>
+
+</style>
